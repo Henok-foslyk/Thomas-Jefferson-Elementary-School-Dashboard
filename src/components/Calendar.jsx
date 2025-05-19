7fefe12890d77e037757b1ff25ba938e5459dbde
BenjaminVC <ben@benvancise.com>
2025-05-19 14:22:15 -0700

    added css and updated Calendar component

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,11 +3,13 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { Container } from '@mui/material';
 
+import "../styles/calendar.css";
+
 export default function Calendar() {
     return (
         <>
             <Container>
-                <div className='calendar-heading'>test</div>
+                <div class='calendar-heading'>test</div>
                  <LocalizationProvider dateAdapter={AdapterDayjs}>
                  <DateCalendar />
                 </LocalizationProvider>
