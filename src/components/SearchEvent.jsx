76d1de3fc9602ec9d2b3de98f4db095fece3d817
BenjaminVC <ben@benvancise.com>
2025-05-20 14:09:05 -0700

    updated calendar dashboard, added SearchEvent.jsx

---
+++ src/components/SearchEvent.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";   
+
+export default function SearchEvent() {
+    const [search, setSearch] = useState('');
+
+    return (
+        <>
+        <h1>search event by name</h1>
+        </>
+    )
+}
