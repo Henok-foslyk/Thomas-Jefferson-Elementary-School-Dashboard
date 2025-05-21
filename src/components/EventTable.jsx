33f02e88df568d04001f0749a018bc152cb79605
BenjaminVC <ben@benvancise.com>
2025-05-21 09:22:19 -0700

    updated eventtable

--- src/components/EventTable.jsx
+++ src/components/EventTable.jsx
@@ -1,13 +1,16 @@
 import { Container, gridClasses, Table, TableContainer } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
-import { useState } from "react";   
+import { useEffect, useState } from "react";   
 import { Paper } from "@mui/material";
+
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 import "../styles/calendar.css"
 
 const columns = [
     { field: 'date', headerName: 'Date', width: 130 },
-    { field: 'event', headerName: 'Event Name', width: 130},
+    { field: 'event-name', headerName: 'Event Name', width: 130},
     { field: 'location', headerName: 'Location', width: 130}
 ]
 
@@ -15,9 +18,38 @@ const paginationModel = { page: 0, pageSize: 5 };
 
 export default function EventTable() {
     const [search, setSearch] = useState('');
-    const [rows, getRows] = useState(null);
+    const [rows, setRows] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
+    useEffect(() => {
+        const getEvents = async () => {
+            setIsLoading(true);
+            try {
+                const querySnapshot = await getDocs(collection(db, "events"));
+                const events = querySnapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
+                setRows(events);
+                
+            }
+            catch(e) {
+                console.error(e);
+            }
+            setIsLoading(false);
+        }
+        getEvents();
+        if (rows) {
+            console.log(rows);
+        }
+        
+    }, [])
 
+    useEffect(() => {
+        if (rows) {
+            console.log(rows);
+        }
+    }, [rows])
 
     return (
         <>
@@ -32,7 +64,6 @@ export default function EventTable() {
                         sx={{ border: 0 }}            
                     />
                 </Paper>
-                
             </Container>
             
         </>
