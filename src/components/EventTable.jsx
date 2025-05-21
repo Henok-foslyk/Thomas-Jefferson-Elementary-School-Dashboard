227f501535e455b612cc295923f468c644249b65
BenjaminVC <ben@benvancise.com>
2025-05-21 14:13:23 -0700

    updated EventTable and added dialog component

--- src/components/EventTable.jsx
+++ src/components/EventTable.jsx
@@ -1,7 +1,9 @@
-import { Container, gridClasses, Table, TableContainer } from "@mui/material";
+import { Button, Container, gridClasses, Table, TableContainer, Dialog } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { useEffect, useState } from "react";   
 import { Paper } from "@mui/material";
+
+import EventEditDialog from "./EventEditDialog";
 
 import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
@@ -14,12 +16,15 @@ const columns = [
     { field: 'location', headerName: 'Location', width: 130}
 ]
 
-const paginationModel = { page: 0, pageSize: 5 };
+const paginationModel = { page: 0, pageSizeOption: 5 };
 
 export default function EventTable() {
     const [search, setSearch] = useState('');
     const [rows, setRows] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    const [message, setMessage] = useState('');
+    const [selectedId, setSelectedId] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
 
     useEffect(() => {
         const getEvents = async () => {
@@ -51,8 +56,16 @@ export default function EventTable() {
         }
     }, [rows])
 
-    const handleSelection = () => {
-        console.log("selected");
+    useEffect(() => {
+        if(message) {
+            console.log(message) 
+        }
+    }, [message])
+
+    const handleEdit = () => {
+        console.log("edited!");
+        setIsEditing(!isEditing);
+        
     }
 
     return (
@@ -64,10 +77,30 @@ export default function EventTable() {
                         columns={columns}
                         initialState={{ pagination: { paginationModel } }}
                         pageSizeOption={[5, 10]}
-                        checkboxSelection
+                        checkboxSelection={false}
                         loading={isLoading}
+                        onRowClick={(params) => { 
+                            if (selectedId === params.id) {
+                                setSelectedId(null);
+                            }
+                            else {
+                                setSelectedId(params.id)} 
+                            }
+                        }
+                        
                         sx={{ border: 0 }}            
-                    />
+                    /><br></br>
+                    {selectedId && (
+                        <Button 
+                            variant="outlined"
+                            onClick={() => handleEdit()}
+                        >
+                            Edit
+                        </Button>
+                    )}
+                    {isEditing && (
+                        <EventEditDialog id={selectedId} />
+                    )}
                 </Paper>
             </Container>
             
