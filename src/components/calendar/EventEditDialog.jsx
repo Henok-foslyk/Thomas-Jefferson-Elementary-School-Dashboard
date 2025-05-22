7139258cc70a7f4d76f82d9b8ba602ef6b9ff42f
davidvillon04 <davidv04@vt.edu>
2025-05-22 17:15:52 -0400

    Update EventEditDialog.jsx

--- src/components/calendar/EventEditDialog.jsx
+++ src/components/calendar/EventEditDialog.jsx
@@ -1,59 +1,62 @@
-import { Dialog, TextField, Button, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Container } from "@mui/material"
-import React, { useEffect, useState } from "react"
+import {
+  Dialog,
+  TextField,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  Container,
+} from "@mui/material";
+import { useEffect } from "react";
 
 export default function EventEditDialog({ id, date, event, location, open, onClose }) {
+  useEffect(() => {
+    console.log("Dialog opened with id:", id);
+  }, [id]);
 
-
-    useEffect(() => {
-        console.log("Dialog opened with id:", id);
-    }, [id]);
-
-    return (
-        <>
-            <Dialog open={open} onClose={onClose}>
-                <DialogContent>
-                    <DialogContentText>
-                        Editing the {event} event located at {location}, scheduled for {date}
-                    </DialogContentText><br></br>
-                        <Container>
-                            <TextField 
-                                autoFocus 
-                                margin="dense"
-                                label="Event"
-                                defaultValue={event} 
-                                fullWidth 
-                                variant="standard"
-                            />
-                            <TextField 
-                                autoFocus 
-                                margin="dense"
-                                label="Location"
-                                defaultValue={location} 
-                                fullWidth 
-                                variant="standard"
-                            />
-                            <TextField 
-                                autoFocus 
-                                margin="dense"
-                                label="Date"
-                                defaultValue={date}
-                                type="date"
-                                fullWidth 
-                                variant="standard"
-                            />
-                        </Container>
-                            
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={onClose}>
-                        Cancel
-                    </Button>
-                    <Button>
-                        Save
-                    </Button>
-                </DialogActions>
-            </Dialog>
-
-        </>
-    )
+  return (
+    <>
+      <Dialog open={open} onClose={onClose}>
+        <DialogContent>
+          <DialogContentText>
+            Editing the {event} event located at {location}, scheduled for {date}
+          </DialogContentText>
+          <br></br>
+          <Container>
+            <TextField
+              autoFocus
+              margin="dense"
+              label="Event"
+              defaultValue={event}
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              label="Location"
+              defaultValue={location}
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              label="Date"
+              defaultValue={date}
+              type="date"
+              fullWidth
+              variant="standard"
+            />
+          </Container>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button>Save</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 }
