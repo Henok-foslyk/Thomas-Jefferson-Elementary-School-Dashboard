f6c8d7b54304c9d89f6af7b8b31a7058c314804c
davidvillon04 <davidv04@vt.edu>
2025-05-22 17:15:15 -0400

    Update Calendar.jsx

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,21 +1,19 @@
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import { Container, Grid } from '@mui/material';
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { Container, Grid } from "@mui/material";
 
 import "../../styles/Calendar.css";
 
 export default function Calendar() {
-    return (
-        <>
-            <Container className="calender-container">
-                <div className='calendar-heading'>test</div>
-                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                 <DateCalendar />
-                </LocalizationProvider>
-            </Container>
-           
-        </>
-        
-    );
+  return (
+    <>
+      <Container className="calender-container">
+        <div className="calendar-heading">Events Calendar</div>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateCalendar />
+        </LocalizationProvider>
+      </Container>
+    </>
+  );
 }
