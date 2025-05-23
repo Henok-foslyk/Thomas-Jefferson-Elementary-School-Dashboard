e51b2c3ab862d02d149d16c0d7526fbadd2c15d5
BenjaminVC <ben@benvancise.com>
2025-05-23 05:46:11 -0700

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";import App from "./App";
 import ClassesDashboard from "./routes/ClassesDashboard";
 import Calendar from "./routes/CalendarDashboard";
-import StudentDirectory from "./routes/StudentDirectory";
+import Students from "./routes/StudentDirectory";
 import Teachers from "./routes/TeacherDirectory";
-// import Grades from "./routes/Grades";
-import EditClass from "./routes/EditClass";
-
-import ErrorFallBack from "./components/ErrorFallBack";
-
-const router = createBrowserRouter([
+import Class from "./routes/Class";
+import Grade from "./routes/Grade";
+import EditGrade from "./routes/EditGrade";
+import EditClass from "./routes/EditClass";import ErrorFallBack from "./components/ErrorFallBack";const router = createBrowserRouter([
   { path: "/", element: <App />, errorElement: <ErrorFallBack /> },
   { path: "/classes", element: <ClassesDashboard />, errorElement: <ErrorFallBack /> },
+  { path: "/class/:id", element: <Class />, errorElement: <ErrorFallBack /> },
+  { path: "/class/edit/:class_id", element: <EditClass />, errorElement: <ErrorFallBack /> },
+  { path: "/grades/:class_id/:student_id", element: <Grade />, errorElement: <ErrorFallBack /> },
+  { path: "/grades/edit/:assignment_id", element: <EditGrade />, errorElement: <ErrorFallBack /> },
   { path: "/calendar", element: <Calendar />, errorElement: <ErrorFallBack /> },
-  { path: "/students", element: <StudentDirectory />, errorElement: <ErrorFallBack /> },
+  { path: "/students", element: <Students />, errorElement: <ErrorFallBack /> },
   { path: "/teachers", element: <Teachers />, errorElement: <ErrorFallBack /> },
-  // { path: "/grades", element: <Grades />, errorElement: <ErrorFallBack /> },
-  { path: "/editclass", element: <EditClass />, errorElement: <ErrorFallBack /> }
 ]);
 
 createRoot(document.getElementById("root")).render(
